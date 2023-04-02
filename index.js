ae1b13f833f56161bbd31da030d449d1216b9ee7
Nestor Felipe <necrifede@gmail.com>
2023-04-02 21:01:13 +0200

    Adding CORS headers

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const { router: userRoutes } = require('./users')
 const { isAuth } = require('./users/middlewares')
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next()
+});
+
 app.use(bodyParser());
 app.use(isAuth)
 
