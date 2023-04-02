c11f59c0c26032c5865065615b6d530c158a8e2b
Nestor Felipe <necrifede@gmail.com>
2023-04-02 21:40:50 +0200

    removing CORS, for testing

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const { router: userRoutes } = require('./users')
 const { isAuth } = require('./users/middlewares')
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  next()
-});
-
 app.use(bodyParser());
 app.use(isAuth)
 
